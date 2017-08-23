7e1a2b0660d17864a61c98172ca2f8e5a45be25c
Christian Jung <chris@bolzchristian.de>
2017-08-23 23:22:47 +0200

    adding some debug code

--- Automate/Development/Infrastructure/VM/Provisioning/StateMachines/Methods.class/__methods__/redhat_customizerequest.rb
+++ Automate/Development/Infrastructure/VM/Provisioning/StateMachines/Methods.class/__methods__/redhat_customizerequest.rb
@@ -9,7 +9,10 @@ $evm.log("info", "Provisioning ID:<#{prov.id}> Provision Request ID:<#{prov.miq_
 
 $evm.root.attributes.sort.each { |k,v| $evm.log("info", "EVM:Root attributes: #{v}: #{k}") }
 
+# read user selected T-Shirt size from dialog elements
 tshirtsize = prov.get_option(:dialog_tshirtsize)
+
+# user can optionally override T-Shirt size
 vm_memory = prov.get_option(:dialog_vm_memory)
 vm_cores = prov.get_option(:dialog_vm_cores)
 
@@ -30,6 +33,7 @@ else
     $evm.log("warn", "Unkonwn T-Shirt Size!")
 end
 
+# if the user selected an override, let's set them now
 if vm_memory.to_i > 0
     prov.set_option(:vm_memory,vm_memory.to_i * 1024)
     $evm.log("info", "Memory Override: #{vm_memory}")
@@ -39,6 +43,8 @@ if vm_cores.to_i > 0
     $evm.log("info", "Cores Override: #{vm_cores}")
 end
 
+# the above code is setting cores per socket, make sure we do not by accident multiply by having too many sockets
 prov.set_option(:number_of_sockets, 1)
 
+# just for debugging purposes we write out the resulting provisioning options
 prov.attributes.sort.each { |k,v| $evm.log("info", "Prov attributes: #{v}: #{k}") }
